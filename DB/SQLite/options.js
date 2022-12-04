2fdcb78ed21f983081e26fd2b43d6475bcdb27e7
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-12-04 16:01:30 -0300

    Comment deleted

--- DB/SQLite/options.js
+++ DB/SQLite/options.js
@@ -9,16 +9,3 @@ const options = {
 module.exports = {
   options,
 };
-
-/* const options = {
-  client: "sqlite3",
-  connection: {
-    filename: "./DB/SQLite/ecommerce.sqlite",
-  },
-  useNullAsDefault: true,
-};
-
-module.exports = {
-  options,
-};
- */
