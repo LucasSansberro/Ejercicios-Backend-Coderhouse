658b914cfe588ac731768d52b372da38add81892
Lucas Sansberro <lucas_sansberro@outlook.com>
2023-04-02 11:48:45 -0300

    Nest installed. Project created

--- Config/env.config.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config.js";
-
-const ENV = {
-  MONGOURL: process.env.URLMONGO,
-  AUTHTOKEN: process.env.AUTHTOKEN,
-};
-
-export default ENV;
--- Router/randoms.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getRandomInfoController, getRandomNumbersController } from "../Controller/randoms.controller.js";
-
-const routerRandoms = new Router();
-
-routerRandoms.get("/info", getRandomInfoController);
-routerRandoms.get("/randomNumber", getRandomNumbersController);
-
-export default routerRandoms;
